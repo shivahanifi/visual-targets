9fde2ed0f8ea7a1ede802fc2fc0ced54434ce8e6
Shiva Hnf <shiva.hnf@gmail.com>
2022-10-10 13:26:26 +0200

    Annotation

--- Demo/VT_Demo_OurData/VT_Input_Data.py
+++
@@ -1,8 +0,0 @@
-import yarp
-
-yarp.Network.init()
-
-class VTInputData(yarp.RFModule):
-    #input port for rgb image
-    self.in_port_scene_image = yarp.BufferedPortImageRgb()
-    self.in_port_scene_image.open()
